89186c087fc976ca3e1fb293d432c3abad16b7b3
Gustavo <gustavo.c.jaccoud@gmail.com>
2025-12-04 19:21:01 -0300

    concertei bug mapa

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -5,8 +5,16 @@
 <script setup>
 import { onMounted, onBeforeUnmount, ref, watch } from 'vue'
 import L from 'leaflet'
-import '/src/lib/leaflet-heat.js' // ✅ plugin baixado manualmente
+import '/src/lib/leaflet-heat.js' 
 import 'leaflet/dist/leaflet.css'
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url),
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url),
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url),
+})
 
 // --- Modos de visualização ---
 const MODE_POINTS = 'points'
